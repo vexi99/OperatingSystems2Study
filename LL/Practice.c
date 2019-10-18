dd60a37a08de8836bc65b6bc658f007495d67a21
David Tilson <davidrtilson@gmail.com>
2019-10-18 13:47:49 +0100

    work on Practice.c

--- LL/Practice.c
+++ LL/Practice.c
@@ -5,17 +5,67 @@
 
 //self-refrenctial structure:
 struct listNode {
-    char value; //user inputted value
+    int value; //user inputted value
     struct listNode *nextPtr; //struct created for the nextPtr in the list.
 };// end struct
 
 typedef struct listNode ListNode;
 //prototypes
 void menu(void);
+void insert(ListNode* *sPtr, int val);
 
 void main()
 {
+    int userVal = 0;
+    ListNode* Head = NULL; //New Node Head created, points to NULL as no other list elements exist at start of program.
+
     menu();
+
+}
+
+void insert(ListNode* *sPtr, int val)
+{
+    ListNode* newPtr = malloc(sizeof(ListNode)); //new struct called newPtr created
+
+    if (newPtr != NULL)
+    {
+        newPtr->value = val; //assign user input value to the structure
+        newPtr->nextPtr = NULL; //nextPtr points to NULL, as
+
+        //create new structs, previousPtr and currentPtr
+        ListNode* previousPtr = NULL;
+        ListNode* currentPtr = NULL; 
+
+        //while loop to find the space to input the value in correctly in ascending order.
+        while(currentPtr != NULL && val > currentPtr->value)
+        {
+            previousPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+        }
+
+        if(previousPtr == NULL)
+        {
+            newPtr->nextPtr = *sPtr;
+            *sPtr = newPtr;
+        }
+        //insert new node between currentPtr and previousPtr
+        else
+        {
+            /* this segment of code inserts newPtr inbetween previousPtr and currentPtr, by making previous point to newPtr and then newPtr point to currentPtr */
+            previousPtr->nextPtr = newPtr;
+            newPtr->nextPtr = currentPtr;
+        }
+        
+
+
+    }
+    else
+    {
+        //Code for failure to create struct.
+        printf("Not enough memory to create the list.");
+    }
+    
+
 }
 
 void menu(void)
